750d3338329199569b7d44e4237abbcf40ea9bda
Lipe <feventurelli@hotmail.com>
2021-06-05 00:22:23 -0300

    Tratamento de erros formulário contato

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -18,13 +18,25 @@ class ContatoController extends Controller
     public function salvar(Request $request){
 
         //Validação de campos do formúlario
-        $request->validate([
-            'nome'=>'required|min:3|max:40',
-            'telefone'=>'required',
-            'email'=>'email',
-            'motivo_contatos_id'=>'required',
-            'mensagem'=>'required|max:2000'
-        ]);
+        $request->validate(
+            [
+                'nome'=>'required|min:3|max:40',
+                'telefone'=>'required',
+                'email'=>'email',
+                'motivo_contatos_id'=>'required',
+                'mensagem'=>'required|max:2000'
+            ],
+            [
+                'nome.required'=>'O campo nome precisa ser preenchido',
+                'nome.min'=>'O campo nome precisa ter no miniomo 3 caracteres',
+                'nome.max'=>'O campo nome pode ter no máximo 40 caracteres',
+                'telefone.required'=>'O campo telefone precisa ser preenchido',
+                'email.email'=>'Digite um e-mail válido',
+                'motivo_contatos_id'=>'Selecione o motivo do seu contato',
+                'mensagem.required'=>'Escreva uma mensagem',
+                'mensagem.max'=>'O campo mensagem pode ter no máximo 2000 caracteres'
+            ]
+        );
 
         SiteContato::create($request->all());
         return redirect()->route('site.index');
