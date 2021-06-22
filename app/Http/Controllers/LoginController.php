60550d017ceb72235883be4f76cf720690a608f6
Lipe <feventurelli@hotmail.com>
2021-06-22 20:53:55 -0300

    Iniciando autenticaão

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class LoginController extends Controller
 {
-    public function index(){
-        return view('site.login', ['titulo'=>'Login']);
+    public function index(Request $request){
+
+        $erro = '';
+        
+        if($request->get('erro')==1){
+            $erro = 'Usuário e/ou senha inválidos';
+        }
+
+        if($request->get('erro')==2){
+            $erro = 'Necessário realizar login para ter acesso a página';
+        }
+
+        return view('site.login', ['titulo'=>'Login', 'erro'=>$erro]);
     }
 
     public function autenticar(Request $request){
@@ -26,5 +38,29 @@ class LoginController extends Controller
 
         $request->validate($regras, $feedback);
 
+        //recuperamos os dados do formulaŕio
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+
+        //Iniciar o model user
+        $user = new User();
+
+        $usuario = $user->where('email', $email)
+                        ->where('password', $password)
+                        ->get()
+                        ->first();
+
+        if(isset($usuario->name)){
+            session_start();
+            $_SESSION['nome'] = $usuario->name;
+            $_SESSION['email'] = $usuario->email;
+
+            return redirect()->route('app.clientes');
+        }else{
+            return redirect()->route('site.login', ['erro'=>1]);
+        }
+
+        
     }
 }
